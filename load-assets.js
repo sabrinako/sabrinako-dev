93827aa42f72b7ef675558d2f2369df6d54e427a
Sabrina Ko <shkteddy@gmail.com>
2021-02-23 09:48:45 -0500

    Updated location of stylesheet load to mitigate FOUC

--- load-assets.js
+++ load-assets.js
@@ -21,7 +21,6 @@ class Head extends HTMLElement {
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" type="image/png" href="src/favicon.png"/>
-        <link rel="stylesheet" href="src/style.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Asap:wght@400;700&family=Fraunces:ital,wght@0,500;0,700;1,700&display=swap" rel="stylesheet">
         <script src="navbar.js"></script>
